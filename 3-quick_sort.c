69f4e683eec01e6d6c15192035110cd864e0c21e
rkbrian <rkbriankong@gmail.com>
2021-08-21 07:30:39 +0000

    seg fault: need to fix the recursion

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,33 +7,61 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t i, j, k = -1, tmp, batman = 0, robin = 0;
-	int judge, jury, executioner;
+	size_t i, j;
 
 	if (array == NULL || size <= 1)
 		return;
-	for (i = 0; i < size; i++)
-		batman = batman + i;
-	for (i = 0; i < size; i++)
+	i = 0, j = size - 1;
+	p_tree(array, i, j, size);
+}
+
+/**
+ * p_tree - generate pivot tree, print array at each time a branch is created
+ */
+void p_tree(int *array, size_t low_p, size_t high_p, size_t size)
+{
+	size_t party;
+
+	if (low_p < high_p)
 	{
-		judge = array[i];
-		for (j = size - 1; array[j] != judge; j = j + k)
-		{
-			jury = array[j];
-			if (judge > jury)
-			{
-				array[i] = jury, array[j] = judge;
-				tmp = i, i = j; j = tmp;
-				k = k * (-1);
-				judge = array[i];
-			}
-		else if (judge == jury)
-		{
-			executioner = judge, robin = robin + i;
-			i++;
-			break;
-		}
-		if (judge == executioner)
-			i++;
+		/* main branch of pivot tree */
+		party = lomuto(array, low_p, high_p, size);
+		/* left branches generated in recursion */
+		p_tree(array, low_p, party - 1, size);
+		print_array(array, size);
+		/* right branches generated in recursion */
+		p_tree(array, party + 1, high_p, size);
+		print_array(array, size);
 	}
+}
+
+/**
+ * lomuto - Lomuto partition, sorting array and then return new array section
+ * @array: unsorted array
+ * @low_p: designated lower point in the array
+ * @high_p: designated higher point in the array
+ * @size: size of array
+ */
+size_t lomuto(int *array, size_t low_p, size_t high_p, size_t size)
+{
+	size_t i, j;
+	int tmp, pivot;
+
+	pivot = array[high_p];
+	i = low_p;
+	for (j = low_p; j < high_p; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			tmp = array[j];
+			array[j] = array[i];
+			array[i] = tmp;
+			i++;
+		}
+	}
+	tmp = array[high_p];
+	array[high_p] = array[i];
+	array[i] = tmp;
+	(void)size;
+	return (i);
 }
