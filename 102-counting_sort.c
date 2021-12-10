27b25d8d1a62995a38cb077e8fe99dcf9920cbed
Ran Kong <rkbriankong@gmail.com>
2021-12-09 21:37:12 -0600

    counting sort: debug on cases of dup val and <= 1 element

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,15 +7,16 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count_arr = NULL, len = (int)size;
-	int i, j = 0, k = 0, zero_flag = 0;
+	int *count_arr = NULL, len = (int)size, i, j, k = 0, ai = 0, zero_flag = 0;
 
+	if (!array || len <= 1)
+		return;
 	for (i = 0; i < len; i++)
 	{
 		if (k < array[i])
 			k = array[i];
-		if (array[i] == 0) /* min=0 conflict with init 0 */
-			zero_flag = 1;
+		if (array[i] == 0)
+			zero_flag++;
 	}
 	count_arr = (int *)malloc(sizeof(int) * (k + 1));
 	if (count_arr == NULL)
@@ -23,19 +24,20 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < k + 1; i++) /* init counting array */
 		count_arr[i] = 0;
 	for (i = 0; i < len; i++)
+		count_arr[array[i]]++;
+	printf("%d", count_arr[0]);
+	for (; zero_flag > 0; zero_flag--)
+		array[ai] = 0, ai++;
+	for (i = 1; i < k + 1; i++)
 	{
-		if (array[i] != 0)
-			count_arr[array[i] - 1] = array[i];
-	}
-	if (zero_flag) /* to control j the iteration before next ordered value */
-		array[j] = 0, j++, zero_flag = 0;
-	for (i = 0; i < k + 1; i++)
-	{
+		count_arr[i] += count_arr[i - 1];
 		if (i > 0)
 			printf(", ");
-		printf("%d", j);
-		if (count_arr[i] > 0)
-			array[j] = count_arr[i], j++;
+		printf("%d", count_arr[i]);
+		/* dup value changes time complexity, but it's asked */
+		/* otherwise, next line would be if(count_arr[i-1]<count_arr[i]) */
+		for (j = count_arr[i - 1]; j < count_arr[i]; j++)
+			array[ai] = i, ai++;
 	}
 	printf("\n");
 	if (count_arr)
